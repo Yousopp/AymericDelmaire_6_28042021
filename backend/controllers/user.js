54c688d13672ee8ccfedd22668d80ee33788edce
Aymeric <aymeric.delmaire@hotmail.com>
2021-06-02 11:06:58 +0200

    Crypted Email loading

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,19 +1,31 @@
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
+const validator = require("email-validator");
 
 exports.signupUser = (req, res, next) => {
-  bcrypt.hash(req.body.password, 10)
-    .then(hash => {
-      const user = new User({
-        email: req.body.email,
-        password: hash
-      });
-      user.save()
-        .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-        .catch(error => res.status(400).json({ error }));
-    })
-    .catch(error => res.status(500).json({ error }));
+  const isValidateEmail = validator.validate(req.body.email)
+  if(!isValidateEmail) {
+    res.writeHead(400, 'Email incorrect !"}', {
+      'content-type': 'application/json'
+    });
+    res.end('Le format de l\'Email est incorrect.');
+  } else {
+    const messageToSearchWith = new Message({ name });
+    messageToSearchWith.encryptFieldsSync();
+    const results = await Message.find({ name: messageToSearchWith.name });
+    bcrypt.hash(req.body.password, 10)
+      .then(hash => {
+        const user = new User({
+          email: req.body.email,
+          password: hash
+        });
+        user.save()
+          .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+          .catch(error => res.status(400).json({ error }));
+      })
+      .catch(error => res.status(500).json({ error }));
+    }
 };
 
 exports.loginUser = (req, res, next) => {
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,11 +1,22 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
+require('mongoose-type-email');
+const mongooseFieldEncryption = require("mongoose-field-encryption").fieldEncryption;
+const dotenv = require('dotenv').config();
 
 const userSchema = mongoose.Schema({
-  email: { type: String, required: true, unique: true },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, "Veuillez entrer votre adresse email"],
+  },
   password: { type: String, required: true}
 });
 
 userSchema.plugin(uniqueValidator);
+userSchema.plugin(mongooseFieldEncryption, { fields: ["email"], secret: process.env.CRYPTO_SECRET_KEY, saltGenerator: function (secret) {
+  return "1234567890123456";
+},
+});
 
 module.exports = mongoose.model('User', userSchema);
